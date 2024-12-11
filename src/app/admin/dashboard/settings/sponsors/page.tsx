8c6692e57f9026bcb1af9047a0f7d8c90637f58e
niraj bava <nirajbava222@gmail.com>
2024-12-11 17:59:43 +0530

    app work completed working with frotend and backend

--- src/app/admin/dashboard/settings/sponsors/page.tsx
+++ src/app/admin/dashboard/settings/sponsors/page.tsx
@@ -2,12 +2,24 @@
 import React from 'react';
 import SponsorAbout from './components/SponsorAbout';
 import SponsorGallery from './components/SponsorGallery';
+import dynamic from 'next/dynamic';
+ 
+const DynamicComponentWithNoSSR1 = dynamic(
+  () => import('./components/SponsorAbout'),
+  { ssr: false }
+)
+ 
+const DynamicComponentWithNoSSR2 = dynamic(
+  () => import('./components/SponsorGallery'),
+  { ssr: false }
+)
+ 
 
 function page() {
   return (
     <div className='w-full h-full flex flex-col gap-10'>
-        <SponsorAbout />
-        <SponsorGallery/>
+        <DynamicComponentWithNoSSR1 />
+        <DynamicComponentWithNoSSR2 />
     </div>
   )
 }
