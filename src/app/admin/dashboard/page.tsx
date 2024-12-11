f0f82a9b6d8f852949440306056cf25ece28898d
niraj bava <nirajbava222@gmail.com>
2024-12-11 16:22:31 +0530

    app work completed working with frotend and backend

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ChartSection from './components/ChartSection'
 import UsersSection from './components/UsersSection'
 
+export const dynamic = 'force-dynamic';
+export const revalidate = 60;
+
 function page() {
   return (
     <div className='min-h-[100vh] px-10 flex justify-between flex-row bg-white'>
--- src/app/admin/dashboard/settings/members/page.tsx
+++ src/app/admin/dashboard/settings/members/page.tsx
@@ -1,20 +1,12 @@
-"use client"
 import React from 'react';
 import Main from './components/Main';
 
-import dynamic from 'next/dynamic'
- 
-const DynamicComponentWithNoSSR = dynamic(
-  () => import('./components/Main'),
-  { ssr: false }
-)
-
-
 
 function page() {
+
   return (
     <div className='flex w-full h-full flex-col'>
-        <DynamicComponentWithNoSSR />
+        <Main/>
     </div>
   )
 }
