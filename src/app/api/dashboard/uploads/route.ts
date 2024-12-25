92e44d7707fbe2ccc8b95f72a275fa4e16c1d258
niraj bava <nirajbava222@gmail.com>
2024-12-25 15:47:24 +0530

    image upload error fixed

--- src/app/api/dashboard/uploads/route.ts
+++ src/app/api/dashboard/uploads/route.ts
@@ -1,19 +1,25 @@
 import { NextRequest, NextResponse } from "next/server";
 import uploadToCloudinary from "@/lib/uploadToCloudinary";
+import sharp from "sharp";
 
 export async function POST(req: NextRequest) {
   try {
     const formData = await req.formData();
     const file = formData.get("file") as File;
 
-    const fileBuffer = await file.arrayBuffer();
-    const mimeType = file.type;
-    const encoding = "base64";
-    const base64Data = Buffer.from(fileBuffer).toString("base64");
+    
+    const arrayBuffer = await file.arrayBuffer();
+    const imageBuffer = Buffer.from(arrayBuffer);
+    console.log("Original Buffer Size:", imageBuffer.length);
 
-    const fileUri = "data:" + mimeType + ";" + encoding + "," + base64Data;
+    const resizedBuffer = await sharp(imageBuffer).resize(500, 500).toBuffer();
+    console.log("Resized Buffer Size:", resizedBuffer.length);
 
-    const res = await uploadToCloudinary(fileUri, file.name);
+    // Step 3: Convert resized buffer to Base64
+    const base64ResizedImage = `data:image/jpeg;base64,${resizedBuffer.toString("base64")}`;
+
+    const res = await uploadToCloudinary(base64ResizedImage, file.name);
+    
 
     if (res.success && res.result) {
       return NextResponse.json({
