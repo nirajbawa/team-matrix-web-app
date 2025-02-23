1bd1e222efedf077cdad79271c57c7cacbaaae85
Niraj Bava <nirajbava222@gmail.com>
2025-02-24 00:58:49 +0530

    build bug fixed 4

--- src/app/admin/dashboard/settings/stories/page.tsx
+++ src/app/admin/dashboard/settings/stories/page.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import StoriesMain from "./components/StoriesMain";
+import dynamic from "next/dynamic";
+
+const DynamicComponentWithNoSSR = dynamic(
+  () => import("./components/Stories"),
+  { ssr: false }
+);
 
 function page() {
   return (
     <div className="w-full h-full p-10">
-      <StoriesMain />
+      <DynamicComponentWithNoSSR />
     </div>
   );
 }
