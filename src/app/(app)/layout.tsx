285c8dc84a0a8fda044a4295ac08f2f65896498d
Niraj Bava <nirajbava222@gmail.com>
2025-02-24 15:13:48 +0530

    production error 8 fixed

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import HomeNavbar from "@/components/navbars/HomeNavbar";
-import ScriptLoader from "./(home)/components/ScriptLoader";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Team Matrix Robotics Club",
@@ -41,10 +41,17 @@ export default function RootLayout({
 }>) {
   return (
     <>
-      <ScriptLoader />
       <div className="w-full overflow-hidden dark:bg-black">
         <HomeNavbar />
         {children}
+        <Script
+          src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r134/three.min.js"
+          strategy="beforeInteractive"
+        />
+        <Script
+          src="https://cdn.jsdelivr.net/npm/vanta@latest/dist/vanta.net.min.js"
+          strategy="beforeInteractive"
+        />
       </div>
     </>
   );
