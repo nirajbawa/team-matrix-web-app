590cf2e9a3212c2bed098e91df09d153458966e6
Niraj Bava <nirajbava222@gmail.com>
2025-02-24 13:13:24 +0530

    production error 7 fixed

--- src/app/(app)/(home)/components/ScriptLoader.tsx
+++ src/app/(app)/(home)/components/ScriptLoader.tsx
@@ -12,24 +12,6 @@ function ScriptLoader() {
         strategy="beforeInteractive"
         src="https://cdn.jsdelivr.net/npm/vanta@latest/dist/vanta.net.min.js"
       />
-      <Script id="script">
-        {`VANTA.NET({
-                    el: "#main-header-bg",
-                    mouseControls: true,
-                    touchControls: true,
-                    gyroControls: false,
-                    minHeight: 200.00,
-                    minWidth: 200.00,
-                    scale: 1.00,
-                    scaleMobile: 1.00,
-                    color: 0xff0000,
-                    points: 7.00,
-                    maxDistance: 18.00,
-                    spacing: 20.00,
-                    showDots: false,
-                    backgroundAlpha: 0,
-                });`}
-      </Script>
     </div>
   );
 }
