e963e80a264df6f5775c5f32e2757c0d50e6f100
Niraj Bava <nirajbava222@gmail.com>
2025-02-24 12:45:05 +0530

    production error 7 fixed

--- src/app/(app)/(home)/components/ScriptLoader.tsx
+++ src/app/(app)/(home)/components/ScriptLoader.tsx
@@ -1,30 +1,17 @@
-"use clinet";
 import Script from "next/script";
 import React from "react";
 
 function ScriptLoader() {
   return (
     <div>
-      <Script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r134/three.min.js" />
-      <Script src="https://cdn.jsdelivr.net/npm/vanta@latest/dist/vanta.net.min.js" />
-      <Script id="script">
-        {`VANTA.NET({
-                    el: "#main-header-bg",
-                    mouseControls: true,
-                    touchControls: true,
-                    gyroControls: false,
-                    minHeight: 200.00,
-                    minWidth: 200.00,
-                    scale: 1.00,
-                    scaleMobile: 1.00,
-                    color: 0xff0000,
-                    points: 7.00,
-                    maxDistance: 18.00,
-                    spacing: 20.00,
-                    showDots: false,
-                    backgroundAlpha: 0,
-                });`}
-      </Script>
+      <Script
+        strategy="beforeInteractive"
+        src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r134/three.min.js"
+      />
+      <Script
+        strategy="beforeInteractive"
+        src="https://cdn.jsdelivr.net/npm/vanta@latest/dist/vanta.net.min.js"
+      />
     </div>
   );
 }
