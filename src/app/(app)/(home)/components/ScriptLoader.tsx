97624b79dd349ad1ee52d8368c95fb297a33522a
Niraj Bava <nirajbava222@gmail.com>
2025-02-24 12:33:37 +0530

    production error 7 fixed

--- src/app/(app)/(home)/components/ScriptLoader.tsx
+++ src/app/(app)/(home)/components/ScriptLoader.tsx
@@ -7,11 +7,11 @@ function ScriptLoader() {
     <div>
       <Script
         src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r134/three.min.js"
-        strategy="beforeInteractive"
+        strategy="afterInteractive"
       />
       <Script
         src="https://cdn.jsdelivr.net/npm/vanta@latest/dist/vanta.net.min.js"
-        strategy="beforeInteractive"
+        strategy="afterInteractive"
       />
     </div>
   );
