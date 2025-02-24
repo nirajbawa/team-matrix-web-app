627b313c85dc81301f630fb95696b24f090e0886
Niraj Bava <nirajbava222@gmail.com>
2025-02-24 22:54:07 +0530

    production error 18 fixed

--- src/lib/createAdmin.ts
+++ src/lib/createAdmin.ts
@@ -3,27 +3,24 @@ import bcrypt from "bcryptjs";
 import UserModel from "../models/User";
 
 export async function createAdmin() {
-  try {
-    await dbConnect();
-    const password = process.env.ADMIN_PASSWORD || "1234578";
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const users = await UserModel.find({ role: "Admin" });
-    if (users.length === 0) {
-      const newUser = new UserModel({
-        username: process.env.ADMIN_USERNAME,
-        email: process.env.ADMIN_EMAIL,
-        password: hashedPassword,
-        verifyCode: "000000",
-        verifyCodeExpiry: new Date(),
-        isVerified: true,
-        role: "Admin",
-      });
+  dbConnect();
+  const password = process.env.ADMIN_PASSWORD || "1234578";
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const users = await UserModel.find({ role: "Admin" });
+  if (users.length === 0) {
+    const newUser = new UserModel({
+      username: process.env.ADMIN_USERNAME,
+      email: process.env.ADMIN_EMAIL,
+      password: hashedPassword,
+      verifyCode: "000000",
+      verifyCodeExpiry: new Date(),
+      isVerified: true,
+      role: "Admin",
+    });
 
-      await newUser.save();
-    }
-  } catch {}
+    await newUser.save();
+  } else {
+  }
 }
 
-try {
-  createAdmin();
-} catch {}
+createAdmin();
