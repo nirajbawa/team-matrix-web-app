443a0357644223d5d33c16e8f7955cebc78a499f
Niraj Bava <nirajbava222@gmail.com>
2025-03-07 13:55:07 +0530

    new sitemap added

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,6 +4,12 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   return [
     {
       url: `${process.env.BASE_URL}`,
-    }
+    },
+    {
+      url: `${process.env.BASE_URL}/members`,
+    },
+    {
+      url: `${process.env.BASE_URL}/login`,
+    },
   ];
 }
